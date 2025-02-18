06dd2ab56b9c267d0925354199c48edaa8a94c4c
himanshi1810 <hgandhi1810@gmail.com>
2025-02-18 09:36:19 +0530

    Practical 01 Done

--- DebuggingApplication/Program.cs
+++ DebuggingApplication/Program.cs
@@ -1,7 +1,15 @@
-string userResponse;
-Console.WriteLine("Enter Your Name :- ");
-userResponse = Console.ReadLine();
-if(Convert.ToBoolean(int.Parse(userResponse)))
+namespace DebuggingApplication
 {
-    Console.WriteLine("The entered value is a integer");
+    internal class DebuggingApplication
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Your User Name Here: ");
+            string userResponse = Console.ReadLine();
+            if (Convert.ToBoolean(int.Parse(userResponse)))
+            {
+                Console.WriteLine("The entered value is a integer");
+            }
+        }
+    }
 }
